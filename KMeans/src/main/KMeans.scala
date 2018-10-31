b9e2b87ccae59cffb63acaedec459245b373bfc3
Nishigaki-taku <43058037+Nishigaki-taku@users.noreply.github.com>
2018-10-31 10:26:51 +0900

    Update KMeans.scala

--- KMeans/src/main/KMeans.scala
+++ KMeans/src/main/KMeans.scala
@@ -1,27 +1,35 @@
 import java.io.File
 import com.github.tototoshi.csv._
-import scala.collection.immutable
 
 object KMeans {
   def main(args: Array[String]): Unit = {
-    val reader = CSVReader.open(new File("IRIS.csv"))
-    val members: Vector[Vector[Double]] = reader.all().tail.map(v => v.map(_.toDouble).toVector).toVector
-    val labels = KMeans(3, data, 50)
+        val reader = CSVReader.open(new File("IRIS.csv"))
+        val data: Vector[Vector[Double]] = reader.all().tail.map(v => v.map(_.toDouble).toVector).toVector
+        println(data.head)
+
+        val labels = kMeans(3, data, 10)
+        println(labels)
+
   }
 
   type Centroids = Vector[Vector[Double]]
-  type Members = Vector[Vector[Double]]
+  type Data      = Vector[Vector[Double]]
+  type Labels    = Vector[Int]
 
-  def KMeans(clusters: Int, members: Vector[Vector[Double]], iteration: Int) = {
+  def kMeans(clusters: Int, data: Vector[Vector[Double]], iteration: Int): Labels = {
+
     lazy val centroids = Vector(
-      Vector(5.0, 3.0, 1.0, 0.5),
-      Vector(6.0, 2.0, 4.0, 1.0),
-      Vector(9.0, 4.0, 7,0, 2.0)
+      Vector(5.0, 3.5, 1.0, 0.1),
+      Vector(5.5, 2.0, 3.8, 1.1),
+      Vector(8.2, 4.2, 7.1, 3.0)
     )
-    val (clustered, _) = MoveCentroid(members, centroids, iteration)
 
+    val (clustered, _) = run(data, centroids, iteration)
+
+    data.map(nearest(_, clustered))
   }
-  def MoveCentroid(members: Members, centroids: Centroids, iteration: Int): (Centroids, Int) = {
+
+  def run(data: Data, centroids: Centroids, iteration: Int): (Centroids, Int) = {
     iteration match {
       case 0 => (centroids, iteration)
       case _ =>
@@ -29,33 +37,30 @@ object KMeans {
         println(labels)
 
         val newCentroids = for {
-          i <= centroids.indices
+          i <- centroids.indices
         } yield {
           centroid(data.zipWithIndex.filter(p => labels(p._2) == i).map(_._1))
         }
         println(newCentroids)
-        MoveCentroid(members, newCentroids.toVector, iteration - 1)
+        println("----------------------------------------------------------------------")
+        run(data, newCentroids.toVector, iteration - 1)
     }
   }
+
 
   def nearest(vec: Vector[Double], centroids: Vector[Vector[Double]]): Int = {
-    val min = centroids.zip.minBy {
+    val m = centroids.zipWithIndex.minBy {
       case (v, _) =>
-        Math.sqrt(vec.zip(v).foldLeft(0d){case(acc, (p, q)) => acc + Math.pow(q - p, 2)})
+        Math.sqrt(vec.zip(v).foldLeft(0d){ case (acc, (p, q)) => acc + Math.pow(q - p, 2) })
     }
-    min._2
+    m._2
   }
 
-  def centroid(members: Vector[Vector[Double]]): Vector[Double] = {
-    members.foldLeft(Vector.fill(members.head.size)(0d)){
-      (acc, v) => v.zip(acc).map{case(d1, d2) => da + d2}
-    }.map{v => v / members.size}
+  def centroid(data: Vector[Vector[Double]]): Vector[Double] = {
+    data.foldLeft(Vector.fill(data.head.size)(0d)){ (acc, v) =>
+      v.zip(acc).map{ case (d1, d2) => d1 + d2 }
+    }.map { v =>
+      v / data.size
+    }
   }
 }
-
-
-
-
-
-
-
