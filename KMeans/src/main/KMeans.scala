54b729d3d14bc75955e2869a426ee6614bea3bff
Nishigaki-taku <43058037+Nishigaki-taku@users.noreply.github.com>
2018-10-31 14:10:06 +0900

    Update KMeans.scala

--- KMeans/src/main/KMeans.scala
+++ KMeans/src/main/KMeans.scala
@@ -3,25 +3,27 @@ import com.github.tototoshi.csv._
 
 object KMeans {
   def main(args: Array[String]): Unit = {
-        val reader = CSVReader.open(new File("IRIS.csv"))
-        val data: Vector[Vector[Double]] = reader.all().tail.map(v => v.map(_.toDouble).toVector).toVector
-      
-        val labels = kMeans(3, data, 10)
-        println(labels)
+    val reader = CSVReader.open(new File("IRIS.csv"))
+    val data: Vector[Vector[Double]] = reader.all().tail.map(v => v.map(_.toDouble).toVector).toVector
+
+    val labels = KMeans(3, data, 10)
+    println(labels) 
+
   }
 
   type Centroids = Vector[Vector[Double]]
   type Data      = Vector[Vector[Double]]
   type Labels    = Vector[Int]
 
-  def kMeans(clusters: Int, data: Vector[Vector[Double]], iteration: Int): Labels = {
+  def KMeans(clusters: Int, data: Data, iteration: Int): Labels = {
     lazy val centroids = Vector(
       Vector(5.0, 3.5, 1.0, 0.1),
       Vector(5.5, 2.0, 3.8, 1.1),
       Vector(8.2, 4.2, 7.1, 3.0)
     )
-    val (clustered, _) = run(data, centroids, iteration)
+    val (clustered: Centroids, _: Int) = MoveCentroid(data, centroids, iteration)
     data.map(nearest(_, clustered))
+
   }
 
   def MoveCentroid(data: Data, centroids: Centroids, iteration: Int): (Centroids, Int) = {
@@ -41,7 +43,7 @@ object KMeans {
     }
   }
 
-  def nearest(vec: Vector[Double], centroids: Vector[Vector[Double]]): Int = {
+  def nearest(vec: Vector[Double], centroids: Centroids): Int = {
     val m = centroids.zipWithIndex.minBy {
       case (v, _) =>
         Math.sqrt(vec.zip(v).foldLeft(0d){ case (acc, (p, q)) => acc + Math.pow(q - p, 2) })
@@ -49,7 +51,7 @@ object KMeans {
     m._2
   }
 
-  def centroid(data: Vector[Vector[Double]]): Vector[Double] = {
+  def centroid(data: Data): Vector[Double] = {
     data.foldLeft(Vector.fill(data.head.size)(0d)){ (acc, v) =>
       v.zip(acc).map{ case (d1, d2) => d1 + d2 }
     }.map { v =>
